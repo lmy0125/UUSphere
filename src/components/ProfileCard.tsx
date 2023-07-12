2261991476bd995a302aef1dc6f84cdd35070025
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-07-11 23:20:54 -0700

    added search for user

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,95 @@
+import type { FC } from 'react';
+import { useCallback, useState } from 'react';
+import toast from 'react-hot-toast';
+import PropTypes from 'prop-types';
+import DotsHorizontalIcon from '@untitled-ui/icons-react/build/esm/DotsHorizontal';
+import {
+	Avatar,
+	Box,
+	Button,
+	Card,
+	IconButton,
+	Link,
+	Stack,
+	SvgIcon,
+	Typography,
+} from '@mui/material';
+import type { Connection } from '@/types/social';
+
+interface SocialConnectionProps {
+	connection: Connection;
+}
+
+const SocialConnection: FC<SocialConnectionProps> = (props) => {
+	const { connection } = props;
+	const [status, setStatus] = useState(connection.status);
+
+	const handleConnectionAdd = useCallback((): void => {
+		setStatus('pending');
+		toast.success('Request sent');
+	}, []);
+
+	const handleConnectionRemove = useCallback((): void => {
+		setStatus('not_connected');
+	}, []);
+
+	const showConnect = status === 'not_connected';
+	const showPending = status === 'pending';
+	const isSelf = status === 'self';
+
+	return (
+		<Card variant="outlined" sx={{ height: '100%' }}>
+			<Stack
+				alignItems="flex-start"
+				direction="row"
+				justifyContent="space-between"
+				spacing={2}
+				sx={{ p: 2 }}>
+				<Stack alignItems="flex-start" direction="row" spacing={2}>
+					<Avatar
+						component="a"
+						href="#"
+						src={connection.avatar}
+						sx={{
+							height: 56,
+							width: 56,
+						}}
+					/>
+					<Box sx={{ flexGrow: 1 }}>
+						<Link color="text.primary" href={`/profile/${connection.id}`} variant="subtitle2">
+							{connection.name}
+						</Link>
+						<Typography color="text.secondary" gutterBottom variant="body2">
+							Freshman, Sixth, Biology
+						</Typography>
+						{/* <Typography color="text.secondary" gutterBottom variant="body2">
+							{connection.commonConnections} connections in common
+						</Typography> */}
+					</Box>
+				</Stack>
+				{isSelf && (
+					<Button onClick={handleConnectionAdd} size="small" variant="outlined">
+						View Profile
+					</Button>
+				)}
+				{showConnect && (
+					<Button onClick={handleConnectionAdd} size="small" variant="outlined">
+						Connect
+					</Button>
+				)}
+				{showPending && (
+					<Button onClick={handleConnectionRemove} size="small" color="inherit">
+						Pending
+					</Button>
+				)}
+			</Stack>
+		</Card>
+	);
+};
+
+SocialConnection.propTypes = {
+	// @ts-ignore
+	connection: PropTypes.object,
+};
+
+export default SocialConnection;
---
+++ src/pages/search.tsx
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import type { GetServerSideProps } from 'next';
+import prisma from '@/lib/prisma';
+import { Layout as DashboardLayout } from '@/layouts/dashboard';
+import type { Page as PageType } from '@/types/page';
+import { Container, Tab, Tabs, Box, Typography } from '@mui/material';
+import type { Connection, ConnectionStatus } from '@/types/social';
+import ProfileCard from '@/components/ProfileCard';
+import { User } from '@prisma/client';
+import { useSession } from 'next-auth/react';
+
+interface TabOption {
+	label: string;
+	value: string;
+}
+const tabs: TabOption[] = [
+	{
+		label: 'All',
+		value: 'all',
+	},
+	{
+		label: 'Courses',
+		value: 'courses',
+	},
+	{
+		label: 'People',
+		value: 'people',
+	},
+	{
+		label: 'Posts',
+		value: 'posts',
+	},
+];
+
+interface SearchResult {
+	users: User[];
+}
+
+const SearchResultPage: PageType<SearchResult> = ({ users }) => {
+	const [currentTab, setCurrentTab] = useState(0);
+	const searchParams = useSearchParams();
+	const { data } = useSession();
+
+	const query = searchParams.get('query');
+
+	const connection: Connection = {
+		id: '123',
+		avatar: '123',
+		commonConnections: 5,
+		name: 'Jack',
+		status: 'not_connected',
+	};
+	return (
+		<Container maxWidth="xl">
+			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+				<Tabs
+					indicatorColor="primary"
+					onChange={(_, value) => setCurrentTab(value)}
+					scrollButtons="auto"
+					sx={{ px: 3 }}
+					textColor="primary"
+					value={currentTab}
+					variant="scrollable">
+					{tabs.map((tab) => (
+						<Tab key={tab.value} label={tab.label} />
+					))}
+				</Tabs>
+			</Box>
+			<TabPanel value={currentTab} index={0}>
+				{users?.map((user) => {
+					let status = 'not_connected';
+					if (user.id == data?.user.id) {
+						status = 'self';
+					}
+					const connection = {
+						id: user.id,
+						avatar: user.image ?? '',
+						commonConnections: 0,
+						name: user.name ?? 'Need a name',
+						status: status as ConnectionStatus,
+					};
+					return (
+						<Box sx={{ mb: 3 }} key={connection.id}>
+							<ProfileCard connection={connection} />
+						</Box>
+					);
+				})}
+			</TabPanel>
+			<TabPanel value={currentTab} index={1}>
+				Item Two
+			</TabPanel>
+			<TabPanel value={currentTab} index={2}>
+				Item Three
+			</TabPanel>
+			<TabPanel value={currentTab} index={3}>
+				Posts
+			</TabPanel>
+		</Container>
+	);
+};
+
+interface TabPanelProps {
+	children?: React.ReactNode;
+	index: number;
+	value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}>
+			{value === index && <Box sx={{ p: 2 }}>{children}</Box>}
+		</div>
+	);
+}
+
+export const getServerSideProps: GetServerSideProps<SearchResult> = async (context) => {
+	const query = context.query.query as string;
+	if (!query) {
+		return {
+			props: { users: [] },
+		};
+	}
+	console.log('query', query);
+	const users = await prisma.user.findMany({
+		where: {
+			OR: [
+				{ email: { contains: query, mode: 'insensitive' } },
+				{ name: { contains: query, mode: 'insensitive' } },
+			],
+		},
+	});
+
+	return {
+		props: { users: JSON.parse(JSON.stringify(users)) },
+	};
+};
+
+SearchResultPage.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default SearchResultPage;
