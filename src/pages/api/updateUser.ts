047c5063c70da7e3c24463e6b1d3d97826962937
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-07-26 18:28:30 -0700

    replaced country with homeland

--- src/pages/api/updateUser.ts
+++ src/pages/api/updateUser.ts
@@ -25,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 					grade: data.grade,
 					college: data.college,
 					major: data.major,
-					country: data.homeland,
+					homeland: data.homeland,
 				},
 			});
 			res.status(200).json(updatedUser);
