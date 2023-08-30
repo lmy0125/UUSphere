e84d1da52259ae4abf763809d3c50f8340682f11
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-08-30 10:26:05 -0700

    added disconnectUser when users leave

--- src/contexts/ChatContext.tsx
+++ src/contexts/ChatContext.tsx
@@ -10,7 +10,10 @@ interface ChatContextType {
 	userChannels: Channel[] | undefined;
 }
 
-const ChatContext = createContext<ChatContextType>({ chatClient: undefined, userChannels: undefined });
+const ChatContext = createContext<ChatContextType>({
+	chatClient: undefined,
+	userChannels: undefined,
+});
 
 const steam_api_key = process.env.NEXT_PUBLIC_STREAMCHAT_KEY as string;
 
@@ -26,24 +29,26 @@ export default function ChatContextProvider({ children }: { children: React.Reac
 			}
 			const client = new StreamChat(steam_api_key);
 			try {
+				const { created_at, ...chatUser } = session.user;
+
 				console.log('session', session.user);
 				// connect user to stream chat
-				await client.connectUser(session.user, session.streamChatToken);
+				await client.connectUser(chatUser, session.streamChatToken);
 				setChatClient(client);
 				console.log('init chat', client);
 				console.log('chat connected');
 			} catch (err) {
-				console.log("Failed to connect stream chat", err);
+				console.log('Failed to connect stream chat', err);
 			}
 		};
-		
+
 		initStreamChat();
-		
+
 		return () => {
-			// if (chatClient) {
-			// 	chatClient.disconnectUser();
-			// 	console.log('chat disconnected');
-			// }
+			if (chatClient) {
+				chatClient.disconnectUser();
+				console.log('chat disconnected');
+			}
 		};
 	}, [session]);
 
