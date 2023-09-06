d341f333fc4f3d03f723032fd366b69d58745e32
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-09-05 22:07:17 -0700

    disconnect user when unmount

--- src/contexts/ChatContext.tsx
+++ src/contexts/ChatContext.tsx
@@ -30,26 +30,24 @@ export default function ChatContextProvider({ children }: { children: React.Reac
 			try {
 				const { created_at, ...chatUser } = session.user;
 
-				console.log('session', session.user);
 				// connect user to stream chat
 				await client.connectUser(chatUser, session.streamChatToken);
 				setChatClient(client);
-				console.log('init chat', client);
-				console.log('chat connected');
 			} catch (err) {
 				console.log('Failed to connect stream chat', err);
 			}
 		};
 
 		initStreamChat();
+	}, [session]);
 
+	useEffect(() => {
 		return () => {
 			if (chatClient) {
 				chatClient.disconnectUser();
-				console.log('chat disconnected');
 			}
 		};
-	}, [session]);
+	}, [chatClient]);
 
 	return (
 		<ChatContext.Provider value={{ chatClient: chatClient, userChannels: userChannels }}>
