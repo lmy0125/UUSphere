970ca26851a0aef6478d8c2b816a5a6ffcadab9a
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-09-17 18:06:07 -0700

    updated user api endpoint

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -4,16 +4,29 @@ import prisma from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	// Check if user is authenticated
-	const session = await getServerSession(req, res, authOptions);
-	if (!session) {
-		return res.status(401).json({ message: 'Unauthorized.' });
+	console.log(req.method);
+	if (req.method === 'GET') {
+		const userId = req.query.id?.toString() ?? '';
+		try {
+			const user = await prisma.user.findUnique({
+				where: {
+					id: userId,
+				},
+			});
+			res.status(200).json(user);
+		} catch (e) {
+			res.status(500).json({ message: 'Failed to get user.' });
+		}
 	}
+	if (req.method === 'PUT') {
+		// Check if user is authenticated
+		const session = await getServerSession(req, res, authOptions);
+		if (!session) {
+			return res.status(401).json({ message: 'Unauthorized.' });
+		}
 
-	const email = session.user?.email ?? '';
-	const data = req.body;
-
-	if (req.method === 'POST') {
+		const email = session.user?.email ?? '';
+		const data = req.body;
 		try {
 			const updatedUser = await prisma.user.update({
 				where: {
@@ -31,12 +44,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			});
 			res.status(200).json(updatedUser);
 		} catch (e) {
-			res.status(500).json({ message: 'Something went wrong' });
+			res.status(500).json({ message: 'Failed to update user.' });
 		}
 	}
 	// HTTP method not supported!
 	else {
-		res.setHeader('Allow', ['POST']);
+		res.setHeader('Allow', ['GET', 'PUT']);
 		res.status(405).json({ message: `HTTP method ${req.method} is not supported.` });
 	}
 }
