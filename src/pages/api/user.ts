edea622c5db5022210e3faf66a3884efc2937706
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-09-17 18:06:40 -0700

    removed console

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -4,7 +4,6 @@ import prisma from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	console.log(req.method);
 	if (req.method === 'GET') {
 		const userId = req.query.id?.toString() ?? '';
 		try {
