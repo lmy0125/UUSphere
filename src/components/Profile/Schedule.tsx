51a56cb13c2d0abc2a669a8569ab2c346a4de64d
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-11-23 00:01:14 -0800

    added tabs for quarter selection

--- src/components/Profile/Schedule.tsx
+++ src/components/Profile/Schedule.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Container, Tab, Tabs } from '@mui/material';
 import ClassScheulde from '@/components/ClassEnrollment/ClassSchedule';
 import Calendar from '@/components/Calendar';
 import { ClassEnrollmentContextProvider } from '@/contexts/ClassEnrollmentContext';
@@ -8,12 +9,28 @@ interface ScheduleProps {
 }
 
 export default function Schedule({ userId }: ScheduleProps) {
+	const [quarterIndex, setQuarterIndex] = useState(0);
+	const quarters = ['WI24', 'FA23'];
+
 	return (
 		<>
+			<Box>
+				<Tabs
+					indicatorColor="primary"
+					onChange={(_, value) => setQuarterIndex(value)}
+					scrollButtons="auto"
+					sx={{ px: 3 }}
+					textColor="primary"
+					value={quarterIndex}
+					variant="scrollable">
+					<Tab key={0} label="WI24" />
+					<Tab key={1} label="FA23" />
+				</Tabs>
+			</Box>
 			<ClassEnrollmentContextProvider>
-				<ClassScheulde userId={userId} />
+				<ClassScheulde userId={userId} quarter={quarters[quarterIndex]} />
 			</ClassEnrollmentContextProvider>
-			<Calendar userId={userId} />
+			<Calendar userId={userId} quarter={quarters[quarterIndex]} />
 		</>
 	);
 }
