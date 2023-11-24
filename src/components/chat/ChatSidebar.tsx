db1e3ccbd260cba5429d6b46bf69601a19859ee5
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-11-24 13:19:35 -0800

    added collapse for side channels

--- src/components/chat/ChatSidebar.tsx
+++ src/components/chat/ChatSidebar.tsx
@@ -3,8 +3,20 @@ import { useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { SvgIcon, Theme } from '@mui/material';
-import { Box, Button, Divider, Drawer, Stack, Typography, useMediaQuery } from '@mui/material';
+import {
+	Box,
+	Button,
+	Collapse,
+	Divider,
+	Drawer,
+	IconButton,
+	Stack,
+	Typography,
+	useMediaQuery,
+} from '@mui/material';
 import EditNoteIcon from '@mui/icons-material/EditNote';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import { paths } from '@/paths';
 // import { useSelector } from 'src/store';
 // import { ChatSidebarSearch } from './chat-sidebar-search';
@@ -28,6 +40,8 @@ export const ChatSidebar: FC<ChatSidebarProps> = (props) => {
 	const { setComposeMode } = useComposeModeContext();
 	const [searchFocused, setSearchFocused] = useState(false);
 	const [searchQuery, setSearchQuery] = useState<string>('');
+	const [classroomOpen, setClassroomOpen] = useState(true);
+	const [personalOpen, setPersonalOpen] = useState(true);
 	const smUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'));
 	const { setShowChannel } = useChatStackContext();
 
@@ -173,40 +187,65 @@ export const ChatSidebar: FC<ChatSidebarProps> = (props) => {
         query={searchQuery}
         results={searchResults}
       /> */}
-			<Stack sx={{ flex: 1, overflowY: 'auto' }}>
-				<Typography variant="subtitle1" sx={{ px: 2, py: 1 }}>
-					Classrooms
-				</Typography>
-
+			<Stack sx={{ overflowY: 'auto' }}>
 				<Box>
-					<ChannelList
-						channelRenderFilterFn={customClassroomChannelFilterFunction}
-						filters={classroomFilter}
-						sort={sort}
-						// showChannelSearch
-						// additionalChannelSearchProps={additionalProps}
-						Preview={CustomChannelPreview}
-						List={CustomChannelList}
-						EmptyStateIndicator={EmptyClassroomList}
-						// LoadingErrorIndicator={CustomErrorIndicator}
-						// LoadingIndicator={CustomLoadingIndicator}
-					/>
+					<Stack
+						direction="row"
+						justifyContent="space-between"
+						alignItems="center"
+						sx={{ px: 2, py: 1 }}>
+						<Typography variant="subtitle1">Classrooms</Typography>
+						<IconButton
+							onClick={() => setClassroomOpen(!classroomOpen)}
+							aria-expanded={classroomOpen}
+							aria-label="show more">
+							{classroomOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+						</IconButton>
+					</Stack>
+
+					<Collapse in={classroomOpen} timeout="auto">
+						<ChannelList
+							channelRenderFilterFn={customClassroomChannelFilterFunction}
+							filters={classroomFilter}
+							sort={sort}
+							// showChannelSearch
+							// additionalChannelSearchProps={additionalProps}
+							Preview={CustomChannelPreview}
+							List={CustomChannelList}
+							EmptyStateIndicator={EmptyClassroomList}
+							// LoadingErrorIndicator={CustomErrorIndicator}
+							// LoadingIndicator={CustomLoadingIndicator}
+						/>
+					</Collapse>
 				</Box>
 
 				<Divider />
-				<Typography variant="subtitle1" sx={{ px: 2, py: 1 }}>
-					Personal
-				</Typography>
 				<Box>
-					<ChannelList
-						channelRenderFilterFn={customMessageChannelFilterFunction}
-						filters={messageFilter}
-						sort={sort}
-						// additionalChannelSearchProps={additionalProps}
-						Preview={CustomChannelPreview}
-						List={CustomChannelList}
-						EmptyStateIndicator={EmptyPersonalMessageList}
-					/>
+					<Stack
+						direction="row"
+						justifyContent="space-between"
+						alignItems="center"
+						sx={{ px: 2, py: 1 }}>
+						<Typography variant="subtitle1">Personal</Typography>
+						<IconButton
+							onClick={() => setPersonalOpen(!personalOpen)}
+							aria-expanded={personalOpen}
+							aria-label="show more">
+							{personalOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+						</IconButton>
+					</Stack>
+
+					<Collapse in={personalOpen} timeout="auto">
+						<ChannelList
+							channelRenderFilterFn={customMessageChannelFilterFunction}
+							filters={messageFilter}
+							sort={sort}
+							// additionalChannelSearchProps={additionalProps}
+							Preview={CustomChannelPreview}
+							List={CustomChannelList}
+							EmptyStateIndicator={EmptyPersonalMessageList}
+						/>
+					</Collapse>
 				</Box>
 			</Stack>
 		</Stack>
