9fc82f011705ad799962ba9dc54e4b62eb689edb
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-02-04 17:24:35 -0800

    revised idleClassroom api

--- src/pages/api/idleClassrooms.ts
+++ src/pages/api/idleClassrooms.ts
@@ -1,6 +1,7 @@
 import prisma from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ClassroomSchedules, ClassroomIdleTimes, TimeInterval } from '@/types/classroom';
+import { Prisma } from '@prisma/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const classroomName = req.query.name?.toString().toUpperCase() ?? '';
@@ -10,61 +11,49 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 	if (req.method === 'GET') {
 		try {
-			const classesFA23 = await prisma.class.findMany({
-				where: {
-					quarter: 'FA23',
-				},
-				select: {
-					sections: { select: { meetings: true } },
-				},
-			});
-
-			const classesWI24 = await prisma.class.findMany({
+			const fetchedData = await prisma.classroomIdleSchedule.findUnique({
 				where: {
 					quarter: 'WI24',
 				},
 				select: {
-					sections: { select: { meetings: true } },
+					data: true,
 				},
 			});
+			if (
+				fetchedData?.data &&
+				typeof fetchedData?.data === 'object' &&
+				Array.isArray(fetchedData?.data)
+			) {
+				const jsonArray = fetchedData?.data as Prisma.JsonArray;
+				// Type converting
+				const classroomIdelTimes: ClassroomIdleTimes[] = jsonArray.map((item) => {
+					const classroomItem = item as ClassroomIdleTimes;
+					return {
+						name: classroomItem.name,
+						idleTimes: classroomItem.idleTimes.map((intervalArray) =>
+							intervalArray.map(
+								(interval) =>
+									({
+										start: interval.start,
+										end: interval.end,
+									} as TimeInterval)
+							)
+						),
+					} as ClassroomIdleTimes;
+				});
 
-			let classroomSchedules: any = {};
-
-			for (const c of classesFA23) {
-				for (const section of c.sections) {
-					for (const meeting of section.meetings) {
-						let { location } = meeting;
-						if (location && location != ' ' && location != 'TBA') {
-							classroomSchedules[location] = [];
-						}
-					}
-				}
+				const filteredResult = filterIdleClassrooms(
+					classroomIdelTimes,
+					classroomName,
+					day,
+					startTime,
+					endTime
+				);
+				res.status(200).json(filteredResult);
+                return;
 			}
 
-			for (const c of classesWI24) {
-				for (const section of c.sections) {
-					for (const meeting of section.meetings) {
-						let { location, sectionId, ...rest } = meeting;
-						if (location && location in classroomSchedules) {
-							classroomSchedules[location].push(rest);
-						} else {
-							if (location && location != ' ' && location != 'TBA') {
-								classroomSchedules[location] = [rest];
-							}
-						}
-					}
-				}
-			}
-
-			const classroomIdelTimes: ClassroomIdleTimes[] = calculateIdleTimes(classroomSchedules);
-			const filteredResult = filterIdleClassrooms(
-				classroomIdelTimes,
-				classroomName,
-				day,
-				startTime,
-				endTime
-			);
-			res.status(200).json(filteredResult);
+			res.status(200).json({});
 		} catch (e) {
 			res.status(500).json({ message: 'Something went wrong' });
 		}
@@ -74,44 +63,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		res.setHeader('Allow', ['GET']);
 		res.status(405).json({ message: `HTTP method ${req.method} is not supported.` });
 	}
-}
-
-function calculateIdleTimes(schedules: ClassroomSchedules): ClassroomIdleTimes[] {
-	const classroomsIdleTimes: ClassroomIdleTimes[] = [];
-
-	for (const classroom in schedules) {
-		const idleTimes: TimeInterval[][] = Array.from({ length: 7 }, () => []); // 7 days in a week
-
-		for (let day = 0; day < 7; day++) {
-			// Monday to Friday
-			const daySchedules = schedules[classroom].filter((schedule) =>
-				schedule.daysOfWeek.includes(day)
-			);
-			daySchedules.sort((a, b) => parseTime(a.startTime) - parseTime(b.startTime));
-
-			let currentTime = 0; // Start of the day in minutes
-
-			for (const schedule of daySchedules) {
-				const startTime = parseTime(schedule.startTime);
-				const endTime = parseTime(schedule.endTime);
-
-				if (startTime - currentTime > 10) {
-					// Check if idle time is greater 10 minutes
-					idleTimes[day].push({ start: formatTime(currentTime), end: formatTime(startTime) });
-				}
-				currentTime = endTime;
-			}
-
-			if (24 * 60 - currentTime > 10) {
-				// If there's idle time at the end of the day longer than 10 minutes
-				idleTimes[day].push({ start: formatTime(currentTime), end: '24:00' });
-			}
-		}
-
-		classroomsIdleTimes.push({ name: classroom, idleTimes });
-	}
-	classroomsIdleTimes.sort((a, b) => a.name.localeCompare(b.name));
-	return classroomsIdleTimes;
 }
 
 function filterIdleClassrooms(
@@ -139,7 +90,7 @@ function filterIdleClassrooms(
 			const idleStartTime = parseTime(idleTime.start);
 			const idleEndTime = parseTime(idleTime.end);
 
-			if (!startInputTime || startInputTime >= idleStartTime && endInputTime <= idleEndTime) {
+			if (!startInputTime || (startInputTime >= idleStartTime && endInputTime <= idleEndTime)) {
 				idleClassrooms.push(classroomInfo);
 				break; // Break out of the loop if a matching idle period is found
 			}
