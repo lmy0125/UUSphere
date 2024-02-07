dbb17e0b84530e5683ac976ec0fe8d07ac300aef
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-02-07 03:09:37 -0800

    message single user in Composer

--- src/components/chat/Composer.tsx
+++ src/components/chat/Composer.tsx
@@ -2,6 +2,7 @@ import React, { HTMLAttributes, useState } from 'react';
 import {
 	MessageInput,
 	MessageToSend,
+	QuotedMessagePreview,
 	useChannelActionContext,
 	useChatContext,
 } from 'stream-chat-react';
@@ -14,16 +15,18 @@ import {
 	Typography,
 	ListItem,
 	ListItemAvatar,
+	ListItemButton,
 	ListItemText,
 	AutocompleteRenderGetTagProps,
 } from '@mui/material';
 import { useComposeModeContext } from '@/contexts/ComposeModeContext';
+import { useChatStackContext } from '@/contexts/ChatStackContext';
 import axios from 'axios';
 import useSWR from 'swr';
 import { User } from '@prisma/client';
 import UserAvatar from '@/components/UserAvatar';
 import BackButton from '@/components/chat/BackButton';
-import { useChatStackContext } from '@/contexts/ChatStackContext';
+import { ImageDropzone, FileUploadButton } from 'react-file-utils';
 
 export default function Composer() {
 	const [input, setInput] = useState('');
@@ -36,6 +39,17 @@ export default function Composer() {
 	const { data: allUsers, isLoading } = useSWR<User[]>(`/api/users`, fetcher);
 	// filter out the user themselves so they can't send message to themselves
 	const options = allUsers?.filter((user) => user.id != client.user?.id);
+
+	const handleMessageUser = async (user: User) => {
+		if (client.user && user) {
+			const channel = client.channel('messaging', {
+				members: [client.user.id, user.id],
+			});
+			await channel.watch();
+			setComposeMode(false);
+			setActiveChannel(channel);
+		}
+	};
 
 	// Function to handle input value change
 	const handleInputChange = (
@@ -50,9 +64,9 @@ export default function Composer() {
 
 	const renderOption = (props: HTMLAttributes<HTMLLIElement>, option: User) => {
 		return (
-			<ListItem key={option.id} {...props}>
+			<ListItem key={option.id} {...props} onClick={(e) => handleMessageUser(option)}>
 				<ListItemAvatar>
-					<UserAvatar userId={option.id} />
+					<UserAvatar userId={option.id} size={36} />
 				</ListItemAvatar>
 				<ListItemText
 					primary={option.name}
@@ -83,7 +97,7 @@ export default function Composer() {
 		return options.filter(
 			(option) =>
 				option.name.toLowerCase().includes(inputValue.toLowerCase()) ||
-				option.email.toLowerCase().includes(inputValue.toLowerCase())
+				option.email.split('@')[0].toLowerCase().includes(inputValue.toLowerCase())
 		);
 	};
 
@@ -123,7 +137,7 @@ export default function Composer() {
 				<Typography color="text.secondary" sx={{ mx: 1 }} variant="body2">
 					To:
 				</Typography>
-				<Autocomplete
+				{/* <Autocomplete
 					multiple
 					filterSelectedOptions
 					value={recipients}
@@ -149,11 +163,99 @@ export default function Composer() {
 							value={input}
 						/>
 					)}
+				/> */}
+				<Autocomplete
+					options={options ?? []}
+					getOptionKey={(option) => option.id}
+					getOptionLabel={(_) => 'Creating...'}
+					open={shouldOpen}
+					onInputChange={handleInputChange}
+					sx={{
+						width: '100%',
+						'.MuiInputBase-input': { height: '24px' },
+					}}
+					filterOptions={filterOptions}
+					renderOption={renderOption}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							variant="outlined"
+							placeholder="Name or email"
+							value={input}
+						/>
+					)}
 				/>
 			</Box>
 
 			<Box sx={{ flexGrow: 1 }} />
-			<MessageInput grow disableMentions overrideSubmitHandler={overrideSubmitHandler} />
+
+			{/* <CustomMessageInput /> */}
+			{/* <MessageInput grow disableMentions overrideSubmitHandler={overrideSubmitHandler} /> */}
 		</Box>
 	);
 }
+
+import {
+	ChatAutoComplete,
+	EmojiIconLarge,
+	EmojiPicker,
+	SendButton,
+	Tooltip,
+	useMessageInputContext,
+	useTranslationContext,
+} from 'stream-chat-react';
+
+export const CustomMessageInput = () => {
+	const { t } = useTranslationContext();
+
+	const {
+		closeEmojiPicker,
+		emojiPickerIsOpen,
+		handleEmojiKeyDown,
+		handleSubmit,
+		openEmojiPicker,
+	} = useMessageInputContext();
+
+	return (
+		<div className="str-chat__message-input">
+			<div className="str-chat__message-input-inner">
+				<ImageDropzone>
+					<div className="str-chat__file-input-container">
+						<FileUploadButton
+							handleFiles={function (files: FileList | File[]): void {
+								throw new Error('Function not implemented.');
+							}}
+						/>
+					</div>
+					{/* <QuotedMessagePreview quotedMessage={undefined} /> */}
+					{/* <UploadsPreview /> */}
+
+					<div className="str-chat__message-textarea-container">
+						<ChatAutoComplete />
+						<EmojiPicker />
+					</div>
+
+					<SendButton sendMessage={handleSubmit} />
+				</ImageDropzone>
+				{/* <div className="str-chat__input-flat--textarea-wrapper">
+					<ChatAutoComplete />
+					<div className="str-chat__emojiselect-wrapper">
+						<Tooltip>
+							{emojiPickerIsOpen ? 'Close emoji picker' : 'Open emoji picker'}
+						</Tooltip>
+						<span
+							className="str-chat__input-flat-emojiselect"
+							onClick={emojiPickerIsOpen ? closeEmojiPicker : openEmojiPicker}
+							onKeyDown={handleEmojiKeyDown}
+							role="button"
+							tabIndex={0}>
+							<EmojiIconLarge />
+						</span>
+					</div>
+					<EmojiPicker />
+				</div>
+				<SendButton sendMessage={handleSubmit} /> */}
+			</div>
+		</div>
+	);
+};
