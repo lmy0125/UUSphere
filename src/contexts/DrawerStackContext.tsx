31815396f4406d2efef8620709dd4154c7737659
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-06-08 14:06:36 -0700

    commented out unused files

--- src/contexts/DrawerStackContext.tsx
+++ src/contexts/DrawerStackContext.tsx
@@ -1,57 +1,57 @@
-import React, { createContext, useContext, useReducer } from 'react';
+// import React, { createContext, useContext, useReducer } from 'react';
 
-type DrawerComponent = React.ComponentType;
+// type DrawerComponent = React.ComponentType;
 
-interface DrawerStackState {
-	stack: DrawerComponent[];
-}
+// interface DrawerStackState {
+// 	stack: DrawerComponent[];
+// }
 
-interface DrawerAction {
-	type: string;
-	payload?: DrawerComponent;
-}
+// interface DrawerAction {
+// 	type: string;
+// 	payload?: DrawerComponent;
+// }
 
-interface DrawerStackContextType {
-	state: DrawerStackState;
-	dispatch: React.Dispatch<DrawerAction>;
-}
+// interface DrawerStackContextType {
+// 	state: DrawerStackState;
+// 	dispatch: React.Dispatch<DrawerAction>;
+// }
 
-const DrawerStackContext = createContext<DrawerStackContextType | undefined>(undefined);
+// const DrawerStackContext = createContext<DrawerStackContextType | undefined>(undefined);
 
-const DRAWER_ACTIONS = { PUSH: 'push', POP: 'pop', REPLACE: 'replace' };
+// const DRAWER_ACTIONS = { PUSH: 'push', POP: 'pop', REPLACE: 'replace' };
 
-const DrawerStackContextProvider = ({ children }: { children: React.ReactNode }) => {
-	const initialState: DrawerStackState = {
-		stack: [],
-	};
-	const reducer = (state: DrawerStackState, action: DrawerAction) => {
-		switch (action.type) {
-			case DRAWER_ACTIONS.PUSH:
-				return { stack: [...state.stack, action.payload] } as DrawerStackState;
-			case DRAWER_ACTIONS.POP:
-				return { stack: state.stack.slice(0, -1) } as DrawerStackState;
-			case DRAWER_ACTIONS.REPLACE:
-				return { stack: [...state.stack.slice(0, -1), action.payload] } as DrawerStackState;
-			default:
-				return state;
-		}
-	};
+// const DrawerStackContextProvider = ({ children }: { children: React.ReactNode }) => {
+// 	const initialState: DrawerStackState = {
+// 		stack: [],
+// 	};
+// 	const reducer = (state: DrawerStackState, action: DrawerAction) => {
+// 		switch (action.type) {
+// 			case DRAWER_ACTIONS.PUSH:
+// 				return { stack: [...state.stack, action.payload] } as DrawerStackState;
+// 			case DRAWER_ACTIONS.POP:
+// 				return { stack: state.stack.slice(0, -1) } as DrawerStackState;
+// 			case DRAWER_ACTIONS.REPLACE:
+// 				return { stack: [...state.stack.slice(0, -1), action.payload] } as DrawerStackState;
+// 			default:
+// 				return state;
+// 		}
+// 	};
 
-	const [state, dispatch] = useReducer(reducer, initialState);
+// 	const [state, dispatch] = useReducer(reducer, initialState);
 
-	return (
-		<DrawerStackContext.Provider value={{ state, dispatch }}>
-			{children}
-		</DrawerStackContext.Provider>
-	);
-};
+// 	return (
+// 		<DrawerStackContext.Provider value={{ state, dispatch }}>
+// 			{children}
+// 		</DrawerStackContext.Provider>
+// 	);
+// };
 
-const useDrawerStackContext = () => {
-	const context = useContext(DrawerStackContext);
-	if (!context) {
-		throw new Error('useNavigation must be used within a NavigationProvider');
-	}
-	return context;
-};
+// const useDrawerStackContext = () => {
+// 	const context = useContext(DrawerStackContext);
+// 	if (!context) {
+// 		throw new Error('useNavigation must be used within a NavigationProvider');
+// 	}
+// 	return context;
+// };
 
-export { DRAWER_ACTIONS, DrawerStackContextProvider, useDrawerStackContext };
+// export { DRAWER_ACTIONS, DrawerStackContextProvider, useDrawerStackContext };
--- src/pages/myClasses.tsx
+++ src/pages/myClasses.tsx
@@ -1,56 +1,56 @@
-import React, { useState } from 'react';
-import type { Page as PageType } from '@/types/page';
-import { Layout as DashboardLayout } from '@/layouts/dashboard';
-import { Box, CircularProgress, Container, Stack, Typography, Tabs, Tab } from '@mui/material';
-import axios from 'axios';
-import useSWR from 'swr';
-import { useSession } from 'next-auth/react';
-import { availableQuarters } from '@/constants/availableQuarters';
+// import React, { useState } from 'react';
+// import type { Page as PageType } from '@/types/page';
+// import { Layout as DashboardLayout } from '@/layouts/dashboard';
+// import { Box, CircularProgress, Container, Stack, Typography, Tabs, Tab } from '@mui/material';
+// import axios from 'axios';
+// import useSWR from 'swr';
+// import { useSession } from 'next-auth/react';
+// import { availableQuarters } from '@/constants/availableQuarters';
 
-const TemplatePage: PageType = () => {
-	const [classIndex, setClassIndex] = useState(0);
-	const [quarter, setQuarter] = useState();
-	const { data: session } = useSession();
-	const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-	const { data } = useSWR(
-		`/api/enrolledClasses?userId=${session?.user.id}&quarter=${availableQuarters[2]}`,
-		fetcher
-	);
-	console.log(data);
-	return (
-		<>
-			<Box
-				component="main"
-				sx={{
-					flexGrow: 1,
-					pb: 8,
-					mt: 1,
-				}}>
-				<Container maxWidth="lg">
-					<Stack>
-						<Typography variant="h4">My Classses</Typography>
-						{/* <Typography variant="subtitle2">
-							Share anything, anytime, anywhere. Your voice, your platform.
-						</Typography> */}
-					</Stack>
-					<Tabs
-						indicatorColor="primary"
-						onChange={(_, value) => setClassIndex(value)}
-						scrollButtons="auto"
-						sx={{ px: 3 }}
-						textColor="primary"
-						value={classIndex}
-						variant="scrollable">
-						{/* {data?.classes?.map((c, index) => (
-							<Tab key={index} label={c.code} />
-						))} */}
-					</Tabs>
-				</Container>
-			</Box>
-		</>
-	);
-};
+// const TemplatePage: PageType = () => {
+// 	const [classIndex, setClassIndex] = useState(0);
+// 	const [quarter, setQuarter] = useState();
+// 	const { data: session } = useSession();
+// 	const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+// 	const { data } = useSWR(
+// 		`/api/enrolledClasses?userId=${session?.user.id}&quarter=${availableQuarters[2]}`,
+// 		fetcher
+// 	);
+// 	console.log(data);
+// 	return (
+// 		<>
+// 			<Box
+// 				component="main"
+// 				sx={{
+// 					flexGrow: 1,
+// 					pb: 8,
+// 					mt: 1,
+// 				}}>
+// 				<Container maxWidth="lg">
+// 					<Stack>
+// 						<Typography variant="h4">My Classses</Typography>
+// 						{/* <Typography variant="subtitle2">
+// 							Share anything, anytime, anywhere. Your voice, your platform.
+// 						</Typography> */}
+// 					</Stack>
+// 					<Tabs
+// 						indicatorColor="primary"
+// 						onChange={(_, value) => setClassIndex(value)}
+// 						scrollButtons="auto"
+// 						sx={{ px: 3 }}
+// 						textColor="primary"
+// 						value={classIndex}
+// 						variant="scrollable">
+// 						{/* {data?.classes?.map((c, index) => (
+// 							<Tab key={index} label={c.code} />
+// 						))} */}
+// 					</Tabs>
+// 				</Container>
+// 			</Box>
+// 		</>
+// 	);
+// };
 
-TemplatePage.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+// TemplatePage.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
-export default TemplatePage;
+// export default TemplatePage;
