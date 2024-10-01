0737d8c68fb211ecb4b2e6070272f1c331ded594
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-09-30 17:19:02 -0700

    added api for getting a specific class with classId

---
+++ src/pages/api/class/[classId].ts
@@ -0,0 +1,25 @@
+import { getServerSession } from 'next-auth/next';
+import prisma from '@/lib/prisma';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const { method, query } = req;
+	const { classId } = query;
+	if (method === 'GET') {
+		try {
+			const c = await prisma.class.findUnique({
+				where: { id: classId as string },
+				select: { sections: { include: { meetings: true } } },
+			});
+
+			res.status(200).json(c?.sections);
+		} catch (e) {
+			res.status(500).json({ message: 'Failed to fetch class: ' + e });
+		}
+	}
+	// HTTP method not supported!
+	else {
+		res.setHeader('Allow', ['GET']);
+		res.status(405).json({ message: `HTTP method ${req.method} is not supported.` });
+	}
+}
