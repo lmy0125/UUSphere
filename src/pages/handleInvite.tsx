db4352e5305e778a72aad587fcb13122eefc9990
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-10-02 04:28:26 -0700

    handled cases when users already joined class or are not students in handleInvite

--- src/pages/handleInvite.tsx
+++ src/pages/handleInvite.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 import { ChannelFilters, DefaultGenerics, StreamChat } from 'stream-chat';
 import prisma from '@/lib/prisma';
 
 const MiddlewarePage = () => {
-	// Since all operations are handled server-side, this component can be empty or show a loading indicator
 	return null;
 };
 
@@ -25,19 +23,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 	const { query } = context; // Extract query parameters from context
 	const { c: channelId, s: sectionId } = query;
-	// console.log('channelId:', channelId, 'sectionId:', sectionId);
 
 	try {
-		// Perform your backend operations here
-		// Example: Update the user's last login time in the database
-		// await prisma.user.update({ where: { email: session.user.email }, data: { lastLogin: new Date() } });
 		const serverClient = StreamChat.getInstance(
 			process.env.NEXT_PUBLIC_STREAMCHAT_KEY! as string,
 			process.env.STREAMCHAT_SECRET! as string
 		);
 		// create the user if it is not in the chat db yet
 		const { created_at, emailVerified, ...chatUser } = session.user;
-		await serverClient.connectUser(chatUser, session.streamChatToken);
+		await serverClient.upsertUser(chatUser);
 
 		const handleJoinChannel = async () => {
 			try {
@@ -48,34 +42,52 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 				// add user to the channel
 				await channel.addMembers([session.user.id]); // join corresponding class
-				// join corresponding class
-				await prisma.user.update({
+
+				// check if user is already in the class
+				const user = await prisma.user.findUnique({
 					where: {
 						email: session.user.email,
 					},
-					data: {
-						sections: {
-							connect: {
-								id: sectionId as string,
-							},
-						},
-						classes: {
-							connect: {
-								id: channelId as string,
-							},
-						},
+					select: {
+						classes: true,
 					},
 				});
+				const hasClass = user?.classes?.some((c) => c.id == channelId) ?? false;
+				if (hasClass) {
+					return;
+				} else {
+					// join corresponding class
+					await prisma.user.update({
+						where: {
+							email: session.user.email,
+						},
+						data: {
+							sections: {
+								connect: {
+									id: sectionId as string,
+								},
+							},
+							classes: {
+								connect: {
+									id: channelId as string,
+								},
+							},
+						},
+					});
+				}
 			} catch (error) {
 				console.error('Failed to join channel:', error);
 			}
 		};
-		handleJoinChannel();
+
+		if (session.user.verifiedStudent) {
+			handleJoinChannel();
+		}
 
 		// After completing the operations, redirect the user to the desired page
 		return {
 			redirect: {
-				destination: `/chat?channelId=${channelId}`, // Replace with your desired destination page
+				destination: `/chat?channelId=${channelId}&isNew=${session.user.isNewUser}`, // Replace with your desired destination page
 				permanent: false,
 			},
 		};
